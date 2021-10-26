64a303dcf48f0ff7bd17d157890b5137cf3939b4
Mohsen Esmaeili <Mohsen.es@gmail.com>
2021-10-26 15:53:31 +0330

    Add validation in frontend

--- client-app/application-process/src/elements/components/asset/post/create-asset.ts
+++ client-app/application-process/src/elements/components/asset/post/create-asset.ts
@@ -1,25 +1,38 @@
+import { inject, NewInstance } from 'aurelia-dependency-injection';
 import { DialogController } from 'aurelia-dialog';
-import { inject } from 'aurelia-framework';
+import { ValidationController, ValidationRules } from 'aurelia-validation';
 import { AssetModel } from 'elements/models/asset.model';
 import { PostAssetRequest } from 'elements/models/data-model/post-asset-request.model';
 import { AssetService } from 'elements/services/data-services/asset.service';
 
-@inject(AssetService, DialogController)
+@inject(AssetService, DialogController, NewInstance.of(ValidationController))
 export class CreateAsset
 {
   asset: AssetModel = new AssetModel();
 
-  constructor(private service: AssetService, private controller: DialogController) { }
+  constructor(private service: AssetService, private dialogController: DialogController, private controller: ValidationController)
+  {
+    ValidationRules
+      .ensure((m: AssetModel) => m.name).displayName("Name").required().withMessage("Asset name is required")
+      .ensure((m: AssetModel) => m.symbol).displayName("Symbol").required().withMessage("Asset symbol is required")
+      .on(AssetModel);
+  }
 
   submit(): void
   {
-    const request = new PostAssetRequest();
-    request.Name = this.asset.name;
-    request.Symbol = this.asset.symbol;
-
-    this.service.post(request).then(() =>
+    this.controller.validate().then((res) =>
     {
-      this.controller.ok();
+      if (res.valid)
+      {
+        const request = new PostAssetRequest();
+        request.Name = this.asset.name;
+        request.Symbol = this.asset.symbol;
+
+        this.service.post(request).then(() =>
+        {
+          this.dialogController.ok();
+        });
+      }
     });
   }
 }
--- client-app/application-process/src/elements/components/user/put/edit-user.ts
+++ client-app/application-process/src/elements/components/user/put/edit-user.ts
@@ -1,16 +1,27 @@
+import { inject, NewInstance } from 'aurelia-dependency-injection';
 import { DialogController } from 'aurelia-dialog';
-import { inject } from 'aurelia-framework';
+import { ValidationController, ValidationRules } from 'aurelia-validation';
 import { PutUserRequest } from 'elements/models/data-model/put-user-request.model';
 import { UserModel } from 'elements/models/user.model';
 import { UserService } from 'elements/services/data-services/user.service';
 
-@inject(UserService, DialogController)
+@inject(UserService, DialogController, NewInstance.of(ValidationController))
 export class EditUser
 {
   user: UserModel;
 
-
-  constructor(private service: UserService, private controller: DialogController) { }
+  constructor(private service: UserService, private dialogController: DialogController, private controller: ValidationController)
+  {
+    ValidationRules
+      .ensure((m: UserModel) => m.firstName).displayName("First name").required().minLength(3).maxLength(50).withMessage("FirstName must has at least 3 characters.")
+      .ensure((m: UserModel) => m.lastName).displayName("Last name").required().minLength(3).maxLength(50).withMessage("LastName must has at least 3 characters.")
+      .ensure((m: UserModel) => m.email).displayName("Email").required().email().withMessage("Please specify a valid email.")
+      .ensure((m: UserModel) => m.age).displayName("Age").required().min(19).withMessage("Age must be greater than 18.")
+      .ensure((m: UserModel) => m.street).displayName("Street").required().withMessage("Street is required.")
+      .ensure((m: UserModel) => m.postalCode).displayName("Postal code").required().withMessage("Postal code is required.")
+      .ensure((m: UserModel) => m.houseNumber).displayName("House number").required().withMessage("House number is required")
+      .on(UserModel);
+  }
 
   activate(user: UserModel): void
   {
@@ -19,19 +30,25 @@ export class EditUser
 
   submit(): void
   {
-    const request = new PutUserRequest();
-    request.Id = this.user.id;
-    request.FirstName = this.user.firstName;
-    request.LastName = this.user.lastName;
-    request.Age = this.user.age;
-    request.Email = this.user.email;
-    request.HouseNumber = this.user.houseNumber;
-    request.PostalCode = this.user.postalCode;
-    request.Street = this.user.street;
-
-    this.service.put(request).then(() =>
+    this.controller.validate().then((res) =>
     {
-      this.controller.ok();
+      if (res.valid)
+      {
+        const request = new PutUserRequest();
+        request.Id = this.user.id;
+        request.FirstName = this.user.firstName;
+        request.LastName = this.user.lastName;
+        request.Age = this.user.age;
+        request.Email = this.user.email;
+        request.HouseNumber = this.user.houseNumber;
+        request.PostalCode = this.user.postalCode;
+        request.Street = this.user.street;
+
+        this.service.put(request).then(() =>
+        {
+          this.dialogController.ok();
+        });
+      }
     });
   }
 }
